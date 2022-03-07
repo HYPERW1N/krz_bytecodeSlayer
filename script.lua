0daa9ead7c37e591c39b2bc954ddc77ba4b1e11a
HYPERW1N <100039638+HYPERW1N@users.noreply.github.com>
2022-03-07 10:06:51 -0600

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,33 @@
--- Replace this text with the script you want to deobfuscate
+--[[<roblox!‰ÿ
+
+   =           META$   "       ð      ExplicitAutoJoints   trueINST$   9       ð       Keyframe 	   
+INST#   !       ð      KeyframeSequence        INST(   Ý       ÿ       Pose 3  ˆ!   
+	  pPROP*   @       ð
+	   AttributesSerialize  P     PROP$   y       ÿ
+	   Name   Keyframe HPframePROP(   ^       ð     
+		SourceAssetId  (PROP   1       Ð       Tags
+			 P     PROP/   1       ó       Time }}~~~~|  "*Äfî3  "ªDÌ	 PDÌfî4PROP"           ð      AttributesSerialize    PROP           ð      AuthoredHipHeight€   PROP          à      Loop PROP$   "       ð      Name   Front-Stab_Dummy1PROP          ð      Priority   PROP           ð   
+SourceAssetId       PROP          ð      Tags    PROP(   è       ÿ      AttributesSerialize  ³P     PROPé  F      ð      CFrame 7€?  9þÿ   % Y• €.o?Qˆ½ @Qˆ= 
+	• €Lÿ?Ü×—»% @Ü×—; ð
+	Ì³?ewE½ ˆ_.bwE=Æ³?!Ð± A#Ð1E ð	Iÿ?Rá˜;  «Pá˜»Cÿ?;`¯ A";`/% ð	¯ç?î#ß< ¨Ž­ë#ß¼©ç?"¤£° A$¤£0% ð	÷ó?‰ÿœ¼ 0
+	-‡ÿœ<ñó?YEf° 1\Ef% ß $€KÓr??"¢¾ß @?"¢> ð
+	Îþ|?ªm¾ 
+	0©m>Êþ|?goå± 2ioåß ð	´j}?@¾€{ò¯?>°j}?ÍÒÔ1 AÏÒÔ±º ù?¸?oÑm=À.<üÉq½C[{?¶8>@`00<Ò8¾²Ë{? uÆy?%3ô=SR<¾Uùï½j,~?‹î´<¾¬=>Ñ³±ì‘{?   €Cqœ}?*Hº @*H> 
+	%  Ï 
+ß ð	Bx?,¿ƒ½ Ç.0¿ƒ=8x?<A± 6<Aß <ß ¬ß Ëß mß Aß :ß €zu?DA]>ß qÍè\¾_èyß ‚º<>…—<ß !£÷}ý SÞ‚<Çy‚¼^Œž=ù2?ò¢ºP;¿0‡ž=ß qs/?ö@£Ârö@£=s/ß *ß 6ß ß ð	ëc?A½ îä®=A=Øc?.O1 2ð-O¾€UÐ?û½¾ò­
+	=®¨{?²8>¤Ø‹¼þ7¾€¥÷s?Žv¾ß öHß~>÷àw?|î´<éæ0>ZÚˆ½ß b}ºÙº¶(ß b„gÚ¼bàß œè¿à—Ú¼™ÿ5V×‚½`»ö»/¿ƒ=¨¶}?é7ï= Ñf¯·ï½…=~? µQ~?Ïé=èeò;4bé½aT{?öÚ>u´%<ÿ±¾¥}. q®®??°)B@°)¿ ð
+Ù >è	s¿JÂ ³ã	s?„Ù >Ó;23 òÖ;2³  €? còq?L7§¾½.<2£>")n?µ8>ãÏŒ½³â*Ãÿ2]n>Àyt?QR<¾…gx¿¿Œv>›î´<¼á…=.x1> ššÖšîš 1hHéX±8bé½Vz?°8#à‡X€2Äy?Æô½šóÍ­ >Gè}?{î´<L8>dì6šå  ÿ €Ró?{#¡¼ @{#¡<     €|  _ a | ðe | }y{z | {}{| |
+‚|{~ |}z|#   Â    ð2ŠêP Â \hêW Ó\ Â ê	 ÍpW (       ð­©N  Ž©F Ùº 5©~ S»F V(   \     ðU€ºy \ áí´8 ŽãÑ \`¸¡ 8Ï: !   ð
+ ihih    ji  y|w|  {}  à{y ||w} }y| y/   €A €€€€ A ð@ A <XÐ{ K 1~Ð Ô1Ü ŸÐ9 žÜ —#   î    ð*¥ Û  î` b Mî( 
+	j3 ‘3a ((   Ÿ    ðA-9 ¨ Ðû™ 
+		Ð 4ÿÕ ;¹ >(   { 	  ð gv €  hfz €fw ~ `€zwz x(   . 
+			0Ì g ð q t E  q p™t ®    
+			0Ÿ ! ö J _ ÷ ô  U §™øJÿ i 	  A I W°9  Q Ùá€  [PROP$   ä       ÿ
+			      EasingDirection  ³P     PROP    à       ÿ   
+			EasingStyle  ³P     PROP{   ™      ð      Name   HumanoidRootPart   Torso& öHead   Left Leg	   Right Arm
+				5Leg& ?ArmW ÿg…	«?Leg’
+				ÈÕ&C +PTorsoPROP(   ®      ÿ   
+					SourceAssetId  ÿR PPROP   Ù       ï      Tags  ³P     PROP*   Û       ñ      Weight    
+					     €P     PRNTY   í      ? =  ¦Ÿ	 ¡ Osö ¤¸ B "& "!6 "1F pAV Q`+ °[l  gvsEND  
+					</roblox>
+					-]]
